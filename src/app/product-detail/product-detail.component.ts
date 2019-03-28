9b2d19bcf67a498518d4ab03729db6ed1c241e09
Jevin Fernando <jevinruv@gmail.com>
2019-03-28 11:16:11 +0530

    product detail page added cart add and remove items

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { ActivatedRoute } from '@angular/router';
+import { ShoppingCartService } from '../services/shopping-cart.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -10,18 +11,89 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetailComponent implements OnInit {
 
   id;
-  product = {};
+  product: any = {};
+  shoppingCart: any = {};
+  quantity = 1;
+  inCart = false;
 
   constructor(
     private productService: ProductService,
+    private shoppingCartService: ShoppingCartService,
     private route: ActivatedRoute) { }
 
   ngOnInit() {
 
     this.id = this.route.snapshot.paramMap.get("id");
+
     this.productService.getProduct(this.id).subscribe(result => {
       this.product = result;
     });
+
+    this.shoppingCartService.getCart().subscribe(data => {
+      this.shoppingCart = data;
+      this.init();
+    });
+
   }
+
+  init() {
+    if (this.shoppingCart && this.shoppingCart.shoppingCartProducts) {
+
+      let item = this.shoppingCart.shoppingCartProducts.find(item => item.product.id === this.product.id);
+      if (item) {
+        this.quantity = item.quantity;
+        this.inCart = true;
+      }
+    }
+  }
+
+  valueChange(value) {
+    if (value > 0) {
+      this.quantity = value;
+    }
+    else {
+      this.quantity = 1;
+    }
+  }
+
+  addToCart() {
+
+    let cartId = this.shoppingCartService.getOrCreateCartId();
+
+    let cartItem = {
+      shoppingCartId: cartId,
+      productId: this.product.id,
+      quantity: this.quantity
+    };
+
+    this.shoppingCartService.addOrUpdate(cartItem).subscribe(
+      data => {
+        // console.log(data);
+      },
+      error => {
+        console.error(error);
+      });
+  }
+
+  removeFromCart() {
+
+    let cartId = this.shoppingCartService.getOrCreateCartId();
+
+    let cartItem = {
+      shoppingCartId: cartId,
+      productId: this.product.id,
+      quantity: this.quantity
+    };
+
+    this.shoppingCartService.deleteCartItem(cartItem).subscribe(
+      data => {
+        // console.log(data);
+      },
+      error => {
+        console.error(error);
+      });
+  }
+
+
 
 }
